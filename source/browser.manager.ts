4ec43b872df857ec9006cdd94a5667460b9987e6
ariae21 <yael.fassbind@acryps.com>
2024-05-20 11:27:49 +0200

    Add different viewport sizes

--- source/browser.manager.ts
+++ source/browser.manager.ts
@@ -4,7 +4,11 @@ import { Browser, launch } from "puppeteer";
 export class BrowserManager {
 	private browsers: Browser[] = [];
 	private rotationIndex = 0;
-	private viewportConfiguration = { width: 1920, height: 1080 };
+	private viewportConfiguration = {
+		big: { width: 1920, height: 1080 },
+		medium: { width: 1280, height: 720 },
+		small: { width: 1024, height: 786 }
+	};
 
 	constructor(
 		private size: number = Math.ceil(Math.max(1, cpus().length) / 4)
@@ -18,8 +22,7 @@ export class BrowserManager {
 		for (let index = 0; index < this.size; index++) {
 			this.browsers.push(await launch({
 				headless: false,
-				slowMo: 5,
-				defaultViewport: this.viewportConfiguration
+				defaultViewport: this.viewportConfiguration.small
 			}));
 		}
 	}
@@ -31,7 +34,7 @@ export class BrowserManager {
 
 	async getPage() {
 		const page = await this.browsers[this.rotationIndex].newPage();
-		await page.setViewport(this.viewportConfiguration);
+		await page.setViewport(this.viewportConfiguration.small);
 
 		if (this.rotationIndex + 1 == this.browsers.length) {
 			this.rotationIndex = 0;
