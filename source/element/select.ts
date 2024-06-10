5a3f1006f476f7be49166b21badc15f89a1555bb
ariae21 <yael.fassbind@acryps.com>
2024-06-10 10:35:10 +0200

    Add select element locator format check

--- source/element/select.ts
+++ source/element/select.ts
@@ -5,7 +5,11 @@ import { SingleElement } from "./single";
 export class SelectElement {
 	constructor(
 		private element: SingleElement
-	) {}
+	) {
+		if (!this.element.locator || !this.element.locator.endsWith('select')) {
+			throw new Error(`Select element locator must exist and end with 'select'`);
+		}
+	}
 
 	first(): Feature {
 		const option = this.element.elements('option').first();
